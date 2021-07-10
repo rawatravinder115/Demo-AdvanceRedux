c98943437b2af65eded9a56edba028b2a269c731
rawatravinder115 <rawatravinder115@gmail.com>
2021-07-10 19:29:35 +0530

    add complete project

--- src/App.js
+++ src/App.js
@@ -1,29 +1,106 @@
-import { useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { Fragment, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
+import {sendCartData , fetchCartData} from './store/cart-actions';
+
+// reducers must be pure,side effect free , synchronous functions.
+
+
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
-  const cart =useSelector(state => state.Cart);
-
-  // side effect logic in the component.
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() =>{
-    fetch("https://fir-project-b3903-default-rtdb.firebaseio.com/cart.json",{
-      method: 'PUT', // sending put request overide the existing data with the incoming data. 
-      body: JSON.stringify(cart),
-    });
-  },[cart]);
+    dispatch(fetchCartData());
+  },[dispatch]);
+
+  useEffect(() => {
+    // const sendCartData = async () => {
+    //   dispatch(
+    //     uiActions.showNotification({
+    //       status: 'pending',
+    //       title: 'Sending...',
+    //       message: 'Sending cart data!',
+    //     })
+    //   );
+    //   const response = await fetch(
+    //     'https://fir-project-b3903-default-rtdb.firebaseio.com/cart.json',
+    //     {
+    //       method: 'PUT',
+    //       body: JSON.stringify(cart),
+    //     }
+    //   );
+
+    //   if (!response.ok) {
+    //     throw new Error('Sending cart data failed.');
+    //   }
+
+    //   dispatch(
+    //     uiActions.showNotification({
+    //       status: 'success',
+    //       title: 'Success!',
+    //       message: 'Sent cart data successfully!',
+    //     })
+    //   );
+    // };
+
+    // if (isInitial) {
+    //   isInitial = false;
+    //   return;
+    // }
+
+    // sendCartData().catch((error) => {
+    //   dispatch(
+    //     uiActions.showNotification({
+    //       status: 'error',
+    //       title: 'Error!',
+    //       message: 'Sending cart data failed!',
+    //     })
+    //   );
+    // });
+
+    //************************************************************* */
+
+    if(isInitial){
+        isInitial = false;
+      return ;
+    }
+
+    if(cart.changed){
+      dispatch(sendCartData(cart));
+    }
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
 export default App;
+
+// what is thunk?
+
+// a function that delays an action until later 
+
+// an action creator function that does not return the action but another function which eventually 
+// return the function 
